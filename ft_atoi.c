18b6f4085df565c635bf0b3568554c8e9e0dd15c
Carmen Gomez Rodriguez <cagomez-@c2r5s6.42madrid.com>
2024-12-21 12:50:58 +0100

    sabado 21

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,18 +1,44 @@
-int	ft_atoi(char *str)
-{
-	int result = 0;
-	int sign = 1;
+#include <unistd.h>
 
-	while (*str == ' ' || (*str >= 9 && *str <= 13))
-        str++;
-	if (*str == '-')
-		sign = -1;
-	if (*str == '-' || *str == '+')
-		str++;
-	while (*str >= '0' && *str <= '9')
-	{
-		result = result * 10 + *str - '0';
-		str++;
-	}
-	return (sign * result);
+// int	ft_atoi(char *str)
+// {
+// 	int result = 0;
+// 	int sign = 1;
+
+// 	while (*str == ' ' || (*str >= 9 && *str <= 13))
+//         str++;
+// 	if (*str == '-')
+// 		sign = -1;
+// 	if (*str == '-' || *str == '+')
+// 		str++;
+// 	while (*str >= '0' && *str <= '9')
+// 	{
+// 		result = result * 10 + *str - '0';
+// 		str++;
+// 	}
+// 	return (sign * result);
+// }
+void	ft_putchar(char nbr)
+{
+	write(1, &nbr, 1);
+}
+void	ft_putnbr(int	nbr)
+{
+	int	i;
+
+	i = 0;
+	if (nbr == -2147483648)
+		//write(1, "-2147483648", 11);
+		ft_putchar('-');
+		ft_putchar('2');
+		//ft_putnbr(147483648);
+}
+
+int	main()
+{
+	int	n;
+
+	n = -2147483648;
+	ft_putnbr(n);
+	return (0);
 }
--- ft_atoi_base.c
+++ ft_atoi_base.c
@@ -4,7 +4,7 @@
 int	ft_atoi_base(const char *str, int str_base)
 {
 
-//int result = 0;
+int result = 0;
 int	value = 0;
 //int	i;
 int	sign = 1;
@@ -28,12 +28,24 @@ while (*str)
 	else
 		break;
 	str++;
+	// if (*str >= '0' && *str <= '9')
+	// 	result = *str - '0';
+	// else if (*str >= 'a' && *str <= 'f')
+	// 	result = *str - 'a' + 10;
+	// else if(*str >= 'A' && *str <= 'F')
+	// 	result = *str - 'A' + 10;
+	// else if (result >= str_base)
+	// 	break;
+	// else
+	// 	break;
+	// value = value * str_base + result;
+	// str++;
 }
 return (sign * value);
 }
 int main() {
    const char* binaryStr = "101";      // Base 2
-   const char* octalStr = "25774123";          // Base 8  
+   const char* octalStr = "2577412312578";          // Base 8  
    const char* hexStr = "1FBA23DB";            // Base 16
  
    printf("Binary to Decimal: %d\n", ft_atoi_base(binaryStr, 2));  
